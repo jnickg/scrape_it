12e455c77343327913dbbff1159cce316b4e8ea2
jnickg <jnickg83@gmail.com>
2020-06-12 00:43:08 -0700

    Add unicode font, export to pdf, etc.

--- scrape_it.py
+++ scrape_it.py
@@ -7,11 +7,29 @@ import urllib3
 from urllib.parse import urlsplit
 from tld import get_tld
 import tempfile
+import fpdf
+from fpdf import FPDF
+
+fpdf.set_global("SYSTEM_TTFONTS", join(os.path.dirname(__file__), join('res','fonts')))
 
 def dump_content_pdf(dest='file_dest', content=''):
-  pass
+  dump_content_txt(f"{dest}.txt", content)
+  pdf = FPDF()
+  pdf.add_page()
+  pdf.add_font("NotoSans", style="", fname="NotoSans-Regular.ttf", uni=True)
+  pdf.add_font("NotoSans", style="B", fname="NotoSans-Bold.ttf", uni=True)
+  pdf.add_font("NotoSans", style="I", fname="NotoSans-Italic.ttf", uni=True)
+  pdf.add_font("NotoSans", style="BI", fname="NotoSans-BoldItalic.ttf", uni=True)
+
+  content = content.replace("\t", "")
+  pdf.set_font("NotoSans", size=10)
+  pdf.write(10, content.encode('utf8').decode('utf8'))
+  pdf.output(f"{dest}.pdf")
 
 def dump_content_txt(dest='file_dest', content=''):
+  fh = open(dest, "wb")
+  fh.write(content.encode('utf8'))
+  fh.close()
   pass
 
 def dump_content(dest='file_dest', content='', dest_ext='txt'):
@@ -36,16 +54,11 @@ def scrape_it(url='', mimes=[]):
     is_desired_type = (any(m in link_content_type for m in mimes))
     if (is_desired_type):
       print(f"Found content {link_content_type} at {link_url}")
-      yield (link_url, link_request.content)
+      yield (link_url, link_request.text)
   pass
 
 def scrape_it_main(args):
   urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-  results = {}
-  for url in args.url:
-    rslt = scrape_it(url=url, mimes=args.content)
-    results[url] = rslt
-
   for url in args.url:
     split_base_url = urlsplit(url)
     url_results = join(os.path.dirname(os.path.realpath(__file__)), join("result", os.path.basename(os.path.normpath(split_base_url.path))))
@@ -54,10 +67,7 @@ def scrape_it_main(args):
     for link_url, content in scrape_it(url=url, mimes=args.content):
       split_url = urlsplit(link_url)
       url_fname = os.path.basename(os.path.normpath(split_url.path))
-      fh = open(join(url_results, url_fname), "wb")
-      fh.write(content)
-      fh.close()
-      # make file for link
+      dump_content(dest=join(url_results, url_fname), content=content, dest_ext=args.desttype)
   pass
 
 if __name__ == "__main__":
@@ -65,6 +75,7 @@ if __name__ == "__main__":
   
   parser.add_argument('--url', type=str, nargs='+')
   parser.add_argument('--content', type=str, nargs='+')
+  parser.add_argument('--desttype', type=str)
 
   args = parser.parse_args()
 
