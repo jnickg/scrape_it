bbdb985c3937b8ae234252fbe9e8dd1379df2955
jnickg <jnickg83@gmail.com>
2020-06-14 23:53:53 -0700

    WIP Recursive domain crawling - issue with parallelization of COM For msword (locks?)

--- scrape_it.py
+++ scrape_it.py
@@ -1,15 +1,20 @@
 import os
 from os.path import isfile, join
+import threading
+import concurrent.futures
 import bs4
 import argparse
 import requests
 import urllib3
 from urllib.parse import urlsplit
+from urllib.parse import urljoin
 from tld import get_tld
 import tempfile
 import fpdf
 from fpdf import FPDF
 import win32com.client as win32
+
+CONFIG_RESULT_DIR='results'
 
 fpdf.set_global("SYSTEM_TTFONTS", join(os.path.dirname(__file__), join('res','fonts')))
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -17,6 +22,8 @@ wdFormatPDF = 17
 wdDoNotSaveChanges = 0
 
 com_app_word = None
+
+visited_links = []
 
 def dump_txt_to_pdf(dest='file_dest', content=''):
   dump_txt_to_txt(f"{dest}.txt", content)
@@ -126,26 +133,89 @@ def scrape_it(url='', mimes=[]):
       yield (link_url, link_response.content, link_response.headers['Content-Type'])
   pass
 
-def get_links_for(url='', visited=[], same_domain=True):
+def get_links_for(url='', response=None, visited=[], same_domain=True, domain=None):
   split_url = urlsplit(url)
-  r = requests.get(url, allow_redirects=True, verify=False)
-  soup = bs4.BeautifulSoup(r.content, features='html.parser')
+  print(f"Getting {url}...")
+  if response is None:
+    response = requests.get(url, allow_redirects=True, verify=False)
+  soup = bs4.BeautifulSoup(response.content, features='html.parser')
   all_links = soup.findAll('a')
-  return [l.get("href") for l in all_links if l.get("href") not in visited and (not same_domain or split_url.netloc in l.get("href")]
+  return [urljoin(domain, l.get("href")) for l in all_links if ((l.get("href") not in visited) and (not same_domain or domain is None or domain in urljoin(domain, l.get("href"))))]
 
-def scrape_domain(url='', mimes=[], destfmt='txt'):
-  split_base_url = urlsplit(url)
-  visited_links = []
-  links = get_links_for(url, visited_links, same_domain=True)
-  # TODO
+
+def scrape_url(url='', response=None, mimes=[], destfmt='txt', baseurl=None, working_dir=None, executor=None):
+  global visited_links
+  if (url in visited_links):
+    print(f"{url} | Skipping already-visited link")
+    return 0
+
+  split_url = urlsplit(url)
+
+  if executor is None:
+    raise ValueError("Can't do this!")
+
+  if baseurl is None:
+    baseurl = split_url.netloc
+  domain = f"{split_url.scheme}://{split_url.netloc}"
+  print(f"{url} | Accessing URL from domain {domain}...")
+
+  if working_dir is None:
+    working_dir = os.getcwd()
+
+  # Step into results directory for this URL path
+  results_dir = os.path.basename(os.path.normpath(split_url.path))
+  working_dir = join(working_dir, results_dir)
+  if not os.path.isdir(working_dir):
+    os.makedirs(working_dir, exist_ok=True)
+
+  # Get child links
+  links = get_links_for(url, response, visited_links, same_domain=True, domain=domain)
+  print(f"{url} | Found {len(links)} links total")
+  visited_links.append(url)
+
+  sub_scrape_futures = []
+  # Scrape each link.
+  link_responses = [requests.get(l, allow_redirects=True, verify=False) for l in links]
+
+  #   If the link is for text/html, recurse
+  sub_page_responses = [r for r in link_responses if 'text/html' in r.headers['Content-Type']]
+  sub_page_responses = [r for r in sub_page_responses if r.url not in visited_links]
+  print(f"{url} | Found {len(sub_page_responses)} new responses that are also HTML pages. Creating jobs...")
+  for r in sub_page_responses:
+    f = executor.submit(scrape_url, response=r, url=r.url, mimes=mimes, destfmt=destfmt, baseurl=baseurl, working_dir=working_dir, executor=executor)
+    sub_scrape_futures.append(f)
+
+  #   Otherwise, check if it's one of the mimes we care about
+  content_responses = [r for r in link_responses if r not in sub_page_responses] # Filter known HTML
+  content_responses = [r for r in content_responses if any(m in r.headers['Content-Type'] for m in mimes)] # Filter for MIME
+  content_grabbed = 0
+  print(f"{url} | Found {len(content_responses)} responses that are desired content. Creating jobs...")
+  for r in content_responses:
+    content_type = r.headers['Content-Type']
+    print(f"{url} | Found content {content_type} at {r.url}")
+    url_fname = os.path.basename(os.path.normpath(split_url.path))
+    dump_content(dest=join(working_dir, url_fname), content=r.content, mime=content_type, dest_ext=args.desttype)
+    content_grabbed += 1
+
+  for f in concurrent.futures.as_completed(sub_scrape_futures):
+    content_grabbed += f.result()
+
+  return content_grabbed
 
 def scrape_it_recursive(args):
   global com_app_word
   com_app_word = win32.Dispatch("Word.Application")
   com_app_word.Visible = False
-  for url in args.url:
-    scrape_domain(url=url, mimes=args.content, destfmt=args.desttype)
-    
+  
+  if not os.path.isdir(CONFIG_RESULT_DIR):
+    os.makedirs(CONFIG_RESULT_DIR)
+
+  global visited_links
+  visited_links = []
+  with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+    for url in args.url:
+      print(f"Starting with {url}...")
+      scrape_url(url=url, mimes=args.content, destfmt=args.desttype, working_dir=CONFIG_RESULT_DIR, executor=executor)
 
 
 def scrape_it_main(args):
@@ -172,4 +242,4 @@ if __name__ == "__main__":
 
   args = parser.parse_args()
 
-  scrape_it_main(args)
+  scrape_it_recursive(args)
